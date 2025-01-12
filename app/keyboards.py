a1ecc3c39b2e7fbf980ead92374dc1412eb9ea24
Rudd21 <yackovschool@gmail.com>
2025-01-12 18:45:39 +0200

    Initial commit

---
+++ app/keyboards.py
@@ -0,0 +1,50 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.callbackData import generate_callback
+import os
+
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+def get_files_and_folders(path: str) -> list:
+    """
+    Повертає список файлів і папок у заданій директорії.
+    """
+    try:
+        # Перевіряємо, чи існує папка
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Шлях {path} не знайдено.")
+        
+        # Отримуємо список файлів і папок
+        return os.listdir(path)
+    except Exception as e:
+        print(f"Помилка при отриманні списку файлів і папок: {e}")
+        return []
+
+def create_file_folder_buttons(path: str) -> InlineKeyboardMarkup:
+    """
+    Генерує кнопки для файлів і папок у вказаній директорії.
+    """
+    # Отримуємо список файлів і папок
+    items = get_files_and_folders(path)
+    
+    # Якщо елементів немає, повертаємо пусту клавіатуру
+    if not items:
+        return InlineKeyboardMarkup()
+    
+    # Створюємо список кнопок
+    buttons = []
+    for item in items:
+        full_path = os.path.join(path, item)
+        if os.path.isdir(full_path):
+            # Якщо це папка
+            callback_data = f"folder:{item.replace(' ', '_')}"
+            buttons.append([InlineKeyboardButton(text=f"📁 {item}", callback_data=callback_data)])
+        else:
+            # Якщо це файл
+            callback_data = f"file:{item.replace(' ', '_')}"
+            buttons.append([InlineKeyboardButton(text=f"📄 {item}", callback_data=callback_data)])
+
+    buttons.append([InlineKeyboardButton(text="На Головну", callback_data="back_to_FKEP")])
+
+
+    # Створюємо клавіатуру з кнопками
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
---
+++ main.py
@@ -0,0 +1,83 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.types import InlineKeyboardMarkup, Message ,InlineKeyboardButton, CallbackQuery
+from aiogram import Router
+import os
+
+from app.handlers import router_user, browse_folders, handle_folder_callback
+from app.admin_handlers import router_admin
+from app.database.models import async_main
+import app.keyboards as kb
+
+import app.handlers as h
+
+# admin_id = 5815674712
+admin_id = 1259689667
+
+bot = Bot(token='7605591332:AAHMNis7iWIfxkpYf5SwC6xcJ1t-9cYBtL8')
+dp = Dispatcher()
+
+
+@dp.callback_query()
+async def handle_callback(query: CallbackQuery):
+    """
+    Обробляє callback від кнопок.
+    """
+    print("Callback_Query відпрацював")
+    if query.data == "back_to_FKEP":
+        await h.back_to_FKEP(query)
+    else:
+        await h.handle_folder_callback(query)
+        print(f"Отримано callback_data: {query.data}")
+
+current_dir = h.BASE_DIR
+
+async def main():
+    await async_main()
+    bot = Bot(token='7605591332:AAHMNis7iWIfxkpYf5SwC6xcJ1t-9cYBtL8')
+    # dp = Dispatcher()
+
+    dp.include_router(router_user)
+    dp.include_router(router_admin)
+
+    @dp.message(CommandStart())
+    async def check_user(message: types.Message):
+
+        """Визначення ролі користувача"""
+        if message.from_user.id == admin_id:
+            await message.answer(
+                "Oh, Hello moderator!😏\n"
+                "Команди для керування:\n\n"
+                "/set_path_to_save_document - встановити шлях до папки з якою хочете взаємодіяти\n"
+                "/original_path - вернутись до головної папки\n"
+                "/add_file - додати завдання в папку\n"
+                "/add_folder <Ім'я папки> - додати папку\n"
+                "/delete_item <Iм'я папки> - видалити папку або файл"
+                "/send_message - надіслати інформацію користувачу\n"
+                "/to_send_document - надіслати документ користувачу\n"
+                "/download_all_files - завантажити всі файли які були відправлені на перевірку\n"
+                "/get_user_info - провірити дані користувача\n"
+                "/update_user - видати користувачу бонуси")
+        else:
+            keyboard = await h.browse_folders(message)
+            await message.answer(                
+                "Привіт!🤩\n\n"
+                "Це бот для студентів КЕПу\n"
+                "Тут можна знайти конрольні, лабораторні, самостійні роботи раніше терміну:\n\n"
+                "У вас є можливість надіслати 5 завданнь, яких ще немає в боті і получити - допомогу адміністратора з презинтацією, практичною з графічного дизайну, вебсайтом, грою на python, лабораторна з Технологій\n\n"
+                "Будь ласка не скидуйте завдання від Балабаника, у неї так всі завдання в мудлі\n", reply_markup=keyboard)
+
+        
+        # await h.browse_folders(message)
+    """
+    Головна функція запуску бота.
+    """
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Бот відключений!")
+
