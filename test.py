b9330cc6d69385f5faa3306ea2d64e1a0bf6cbff
Rudd21 <yackovschool@gmail.com>
2025-01-13 13:09:07 +0200

    Remove all files

--- test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from aiogram import Bot
-
-async def test_message():
-    bot = Bot(token="7605591332:AAHMNis7iWIfxkpYf5SwC6xcJ1t-9cYBtL8")
-    await bot.send_message(chat_id="1259689667", text="Привіт! Бот працює. 🎉")
-    await bot.close()
-
-asyncio.run(test_message())
