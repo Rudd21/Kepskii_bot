d1a0ed89a73067fc7234a41ef1d4de74e1e0301b
Rudd21 <yackovschool@gmail.com>
2025-01-14 15:10:45 +0200

    remake port 443

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ async def create_app():
 
 if __name__ == "__main__":
     try:
-        web.run_app(create_app(), host="0.0.0.0", port=PORT)
+        web.run_app(create_app(), host="0.0.0.0", port=443)
     except KeyboardInterrupt:
         print("Бот відключений!")
 
